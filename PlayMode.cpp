da3685cb55ce267e1aa6887e64b95fa4252dbb4c
xinyis991105 <xinyis@andrew.cmu.edu>
2020-09-13 17:44:25 +0800

    idk

--- PlayMode.cpp
+++ PlayMode.cpp
@@ -2,11 +2,15 @@
 
 //for the GL_ERRORS() macro:
 #include "gl_errors.hpp"
+#include "load_save_png.hpp"
+#include "read_write_chunk.hpp"
 
 //for glm::value_ptr() :
 #include <glm/gtc/type_ptr.hpp>
 
 #include <random>
+#include <iostream>
+#include <fstream>
 
 PlayMode::PlayMode() {
 	//TODO:
@@ -17,90 +21,94 @@ PlayMode::PlayMode() {
 	//   and check that script into your repository.
 
 	//Also, *don't* use these tiles in your game:
-
-	{ //use tiles 0-16 as some weird dot pattern thing:
-		std::array< uint8_t, 8*8 > distance;
-		for (uint32_t y = 0; y < 8; ++y) {
-			for (uint32_t x = 0; x < 8; ++x) {
-				float d = glm::length(glm::vec2((x + 0.5f) - 4.0f, (y + 0.5f) - 4.0f));
-				d /= glm::length(glm::vec2(4.0f, 4.0f));
-				distance[x+8*y] = std::max(0,std::min(255,int32_t( 255.0f * d )));
-			}
-		}
-		for (uint32_t index = 0; index < 16; ++index) {
-			PPU466::Tile tile;
-			uint8_t t = (255 * index) / 16;
-			for (uint32_t y = 0; y < 8; ++y) {
-				uint8_t bit0 = 0;
-				uint8_t bit1 = 0;
-				for (uint32_t x = 0; x < 8; ++x) {
-					uint8_t d = distance[x+8*y];
-					if (d > t) {
-						bit0 |= (1 << x);
-					} else {
-						bit1 |= (1 << x);
-					}
-				}
-				tile.bit0[y] = bit0;
-				tile.bit1[y] = bit1;
-			}
-			ppu.tile_table[index] = tile;
-		}
+	std::ifstream palettes_stream;
+	std::ifstream tiles_stream;
+	std::vector<PPU466::Palette> palettes;
+	std::vector<PPU466::Tile> tiles;
+	palettes_stream.open("palettes.asset");
+	tiles_stream.open("tiles.asset");
+	read_chunk(palettes_stream, std::string("pale"), &palettes);
+	read_chunk(tiles_stream, std::string("tile"), &tiles);
+	palettes_stream.close();
+	tiles_stream.close();
+	for (int i=0; i < palettes.size(); i++) {
+		// assign each palette to ppu.palette_table
+		// palette 7: for player
+		ppu.palette_table[7-i] = palettes[i]
+		// palette 6: for enemy 1
+		// palette 5: for enemy 2
 	}
+	for (int i=0; i < tiles.size(); i++) {
+		// assign each tile to ppu.tile_table
+		// tiles 32-35: player left
+		tiles[32 + i * 4] = 
+		// tiles 36-39: player right
+		// tiles 40-43: peach left
+		// tiles 44-47: peach right
+		// tiles 48-51: apple left
+		// tiles 52-55: apple right
+
+		// tiles
+	}
+	ppu.palette_table[7] = palettes[0];
+	ppu.tile_table[32] = tiles[0];
+	ppu.tile_table[34] = tiles[1];
+	ppu.tile_table[33] = tiles[2];
+	ppu.tile_table[35] = tiles[3];
 
 	//use sprite 32 as a "player":
-	ppu.tile_table[32].bit0 = {
-		0b01111110,
-		0b11111111,
-		0b11111111,
-		0b11111111,
-		0b11111111,
-		0b11111111,
-		0b11111111,
-		0b01111110,
-	};
-	ppu.tile_table[32].bit1 = {
-		0b00000000,
-		0b00000000,
-		0b00011000,
-		0b00100100,
-		0b00000000,
-		0b00100100,
-		0b00000000,
-		0b00000000,
-	};
+	// ppu.tile_table[32].bit0 = {
+	// 	0b01111110,
+	// 	0b11111111,
+	// 	0b11111111,
+	// 	0b11111111,
+	// 	0b11111111,
+	// 	0b11111111,
+	// 	0b11111111,
+	// 	0b01111110,
+	// };
+	// ppu.tile_table[32].bit1 = {
+	// 	0b00000000,
+	// 	0b00000000,
+	// 	0b00011000,
+	// 	0b00100100,
+	// 	0b00000000,
+	// 	0b00100100,
+	// 	0b00000000,
+	// 	0b00000000,
+	// };
 
 	//makes the outside of tiles 0-16 solid:
-	ppu.palette_table[0] = {
-		glm::u8vec4(0x00, 0x00, 0x00, 0x00),
-		glm::u8vec4(0x00, 0x00, 0x00, 0xff),
-		glm::u8vec4(0x00, 0x00, 0x00, 0x00),
-		glm::u8vec4(0x00, 0x00, 0x00, 0xff),
-	};
+	// ppu.palette_table[0] = {
+	// 	glm::u8vec4(0x00, 0x00, 0x00, 0x00),
+	// 	glm::u8vec4(0x00, 0x00, 0x00, 0xff),
+	// 	glm::u8vec4(0x00, 0x00, 0x00, 0x00),
+	// 	glm::u8vec4(0x00, 0x00, 0x00, 0xff),
+	// };
 
-	//makes the center of tiles 0-16 solid:
-	ppu.palette_table[1] = {
-		glm::u8vec4(0x00, 0x00, 0x00, 0x00),
-		glm::u8vec4(0x00, 0x00, 0x00, 0x00),
-		glm::u8vec4(0x00, 0x00, 0x00, 0xff),
-		glm::u8vec4(0x00, 0x00, 0x00, 0xff),
-	};
+	// //makes the center of tiles 0-16 solid:
+	// ppu.palette_table[1] = {
+	// 	glm::u8vec4(0x00, 0x00, 0x00, 0x00),
+	// 	glm::u8vec4(0x00, 0x00, 0x00, 0x00),
+	// 	glm::u8vec4(0x00, 0x00, 0x00, 0xff),
+	// 	glm::u8vec4(0x00, 0x00, 0x00, 0xff),
+	// };
 
-	//used for the player:
-	ppu.palette_table[7] = {
-		glm::u8vec4(0x00, 0x00, 0x00, 0x00),
-		glm::u8vec4(0xff, 0xff, 0x00, 0xff),
-		glm::u8vec4(0x00, 0x00, 0x00, 0xff),
-		glm::u8vec4(0x00, 0x00, 0x00, 0xff),
-	};
+	// //used for the player:
+	// ppu.palette_table[7] = {
+	// 	glm::u8vec4(0x00, 0x00, 0x00, 0x00),
+	// 	glm::u8vec4(0x00, 0xff, 0xff, 0xff),
+	// 	glm::u8vec4(0x00, 0x00, 0x00, 0xff),
+	// 	glm::u8vec4(0x00, 0x00, 0x00, 0xff),
+	// };
 
-	//used for the misc other sprites:
-	ppu.palette_table[6] = {
-		glm::u8vec4(0x00, 0x00, 0x00, 0x00),
-		glm::u8vec4(0x88, 0x88, 0xff, 0xff),
-		glm::u8vec4(0x00, 0x00, 0x00, 0xff),
-		glm::u8vec4(0x00, 0x00, 0x00, 0x00),
-	};
+	// //used for the misc other sprites:
+	// ppu.palette_table[6] = {
+	// 	glm::u8vec4(0x00, 0x00, 0x00, 0x00),
+	// 	glm::u8vec4(0x88, 0x88, 0xff, 0xff),
+	// 	glm::u8vec4(0x00, 0x00, 0x00, 0xff),
+	// 	glm::u8vec4(0x00, 0x00, 0x00, 0x00),
+	// };
 
 }
 
@@ -195,16 +203,28 @@ void PlayMode::draw(glm::uvec2 const &drawable_size) {
 	ppu.sprites[0].y = int32_t(player_at.y);
 	ppu.sprites[0].index = 32;
 	ppu.sprites[0].attributes = 7;
+	ppu.sprites[1].x = ppu.sprites[0].x + 8;
+	ppu.sprites[1].y = ppu.sprites[0].y;
+	ppu.sprites[1].index = 33;
+	ppu.sprites[1].attributes = 7;
+	ppu.sprites[2].x = ppu.sprites[0].x;
+	ppu.sprites[2].y = ppu.sprites[0].y + 8;
+	ppu.sprites[2].index = 34;
+	ppu.sprites[2].attributes = 7;
+	ppu.sprites[3].x = ppu.sprites[0].x + 8;
+	ppu.sprites[3].y = ppu.sprites[0].y + 8;
+	ppu.sprites[3].index = 35;
+	ppu.sprites[3].attributes = 7;
 
 	//some other misc sprites:
-	for (uint32_t i = 1; i < 63; ++i) {
-		float amt = (i + 2.0f * background_fade) / 62.0f;
-		ppu.sprites[i].x = int32_t(0.5f * PPU466::ScreenWidth + std::cos( 2.0f * M_PI * amt * 5.0f + 0.01f * player_at.x) * 0.4f * PPU466::ScreenWidth);
-		ppu.sprites[i].y = int32_t(0.5f * PPU466::ScreenHeight + std::sin( 2.0f * M_PI * amt * 3.0f + 0.01f * player_at.y) * 0.4f * PPU466::ScreenWidth);
-		ppu.sprites[i].index = 32;
-		ppu.sprites[i].attributes = 6;
-		if (i % 2) ppu.sprites[i].attributes |= 0x80; //'behind' bit
-	}
+	// for (uint32_t i = 1; i < 63; ++i) {
+	// 	float amt = (i + 2.0f * background_fade) / 62.0f;
+	// 	ppu.sprites[i].x = int32_t(0.5f * PPU466::ScreenWidth + std::cos( 2.0f * M_PI * amt * 5.0f + 0.01f * player_at.x) * 0.4f * PPU466::ScreenWidth);
+	// 	ppu.sprites[i].y = int32_t(0.5f * PPU466::ScreenHeight + std::sin( 2.0f * M_PI * amt * 3.0f + 0.01f * player_at.y) * 0.4f * PPU466::ScreenWidth);
+	// 	ppu.sprites[i].index = 32;
+	// 	ppu.sprites[i].attributes = 7;
+	// 	if (i % 2) ppu.sprites[i].attributes |= 0x80; //'behind' bit
+	// }
 
 	//--- actually draw ---
 	ppu.draw(drawable_size);
